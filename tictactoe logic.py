694124f2ee335472b2a9586afd603879c07b7b75
B <kingofdeath2533@yahoo.com>
2022-12-20 16:49:16 +0200

    added exit code + storage (no pickeling)

--- tictactoe logic.py
+++ tictactoe logic.py
@@ -6,6 +6,13 @@ p1= True
 while True:
     print("_______\n|{}|{}|{}|\n|{}|{}|{}|\n|{}|{}|{}|".format(rows[0][0],rows[0][1],rows[0][2],rows[1][0],rows[1][1],rows[1][2],rows[2][0],rows[2][1],rows[2][2]))
     sushi = input("choose (x,y): ").split(",")
+    '''
+    if sushi[0] == "exit":
+        f = open("board_data.txt","w")
+        f.write(rows)
+        f.close()
+        exit()
+    '''
     if p1 == True:
         rows[3-int(sushi[1])][int(sushi[0])-1] = x
         p1 = False
