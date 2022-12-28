2c0933c9e4369a8b28329c90f08e374f545139b4
B <kingofdeath2533@yahoo.com>
2022-12-28 20:32:50 +0200

    yabagoooo

--- test.py
+++ test.py
@@ -23,7 +23,7 @@ def Computer(salmon,max):
                 break
 
 e,x,o,m,n = "_","X","O", ["X","X","X"], ["O","O","O"]
-farags,milk,p1 = "wins","",True
+milk,p1 = "",True
 soy = int(input("\nAt any time Type save to save or exit to exit.\nChoose: \n1. Play against Computer \n2. Play against yourself \n3. Load save\nChoose 1/2/3: "))
 rows = [[e,e,e],[e,e,e],[e,e,e]]
 if soy == 1:
@@ -37,51 +37,54 @@ elif soy == 3:
         p1 = pickle.load(pickle_file)
         milk = pickle.load(pickle_file)
         rows = pickle.load(pickle_file)
-
 while True:
-    print("[y]\n 3 |{}|{}|{}|\n 2 |{}|{}|{}|\n 1 |{}|{}|{}|\n    1 2 3 [x]".format(rows[0][0], rows[0][1], rows[0][2], rows[1][0], rows[1][1], rows[1][2], rows[2][0], rows[2][1], rows[2][2]))
-    ####### WIN CHECK #######
-    boom = 0
-    for i in rows:
-        for j in i:
-            if j == x:
-                boom += 1
-    if Check(1,m):
-        print("Player 1",farags)
-        break
-    elif Check(0,n):
-        print("Player 2",farags)
-        break
-    elif boom == 5:
-        print("  Tie")
-        break
-    ####### inputs #######
-    sushi = input("choose (x,y): ").upper().split(",")
-    ## exit code ##
-    if sushi[0] == "EXIT":
-        exit()
-    elif sushi[0] == "SAVE":
-        with open("board.pk", "wb") as pickle_file:  #####SAVE
-            pickle.dump(soy, pickle_file)
-            pickle.dump(p1, pickle_file)
-            pickle.dump(milk, pickle_file)
-            pickle.dump(rows, pickle_file)
-            print("\tGame Saved!")  # pickling code
+    while True:
+        print("\n[y]\n 3 |{}|{}|{}|\n 2 |{}|{}|{}|\n 1 |{}|{}|{}|\n    1 2 3 [x]\n".format(rows[0][0], rows[0][1], rows[0][2], rows[1][0], rows[1][1], rows[1][2], rows[2][0], rows[2][1], rows[2][2]))
+        ####### WIN CHECK #######
+        boom = 0
+        for i in rows:
+            for j in i:
+                if j == x:
+                    boom += 1
+        if Check(1,m):
+            print("=-=-[x Wins]-=-=\n")
             break
-    a, b = (3 - int(sushi[1])), (int(sushi[0]) - 1)
-    if rows[a][b] == e:
-        if p1 == True:
-            rows[a][b] = x
-            if soy == 1 and milk == x:
-                Computer(o,4)
-            else:
-                p1 = False
-        elif p1 == False:
-            rows[a][b] = o
-            if soy == 1 and milk == o:
-                Computer(x,5)
-            else:
-                p1 = True
-        continue
-    else:
-        print("incorrect input try again")
+        elif Check(0,n):
+            print("=-=-[o Wins]-=-=\n")
+            break
+        elif boom == 5:
+            print("  Tie\n")
+            break
+        ####### inputs #######
+        sushi = input("choose (x,y): ").upper().split(",")
+        ## exit code ##
+        if sushi[0] == "EXIT":
+            exit()
+        elif sushi[0] == "SAVE":
+            with open("board.pk", "wb") as pickle_file:  #####SAVE
+                pickle.dump(soy, pickle_file)
+                pickle.dump(p1, pickle_file)
+                pickle.dump(milk, pickle_file)
+                pickle.dump(rows, pickle_file)
+                print("\tGame Saved!")  # pickling code
+                break
+        a, b = (3 - int(sushi[1])), (int(sushi[0]) - 1)
+        if rows[a][b] == e:
+            if p1 == True:
+                rows[a][b] = x
+                if soy == 1 and milk == x:
+                    Computer(o,4)
+                else:
+                    p1 = False
+            elif p1 == False:
+                rows[a][b] = o
+                if soy == 1 and milk == o:
+                    Computer(x,5)
+                else:
+                    p1 = True
+            continue
+        else:
+            print("incorrect input try again")
+    rows = [[e, e, e], [e, e, e], [e, e, e]]
+    if input("Would you like to play again? (Y/N)").upper() == "N":
+        break
