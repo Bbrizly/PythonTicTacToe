30e7f84722495c3d3f22cbc432f3c26c129156d1
B <kingofdeath2533@yahoo.com>
2022-12-29 22:28:01 +0200

    Final Rendition #3 hopefully bugfree

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-#Bassam Kamal | Mohammed Kamal | Sajed Farag|
+#Bassam Kamal | Mohammed Kamal | Sajed Fara|
 import pickle, random
 def Board():
     print(
@@ -12,26 +12,40 @@ def Verify(list,a,ax,b,bx,c,cx):
 def Check(q,p):
     if Verify(rows, 0, 0, 1, 0, 2, 0) == q or Verify(rows, 0, 1, 1, 1, 2, 1) == q or Verify(rows, 0, 2, 1, 2, 2, 2) == q or Verify(rows, 0, 0, 1, 1, 2, 2) == q or Verify(rows, 0, 2, 1, 1, 2, 0) == q or rows[1] == p or rows[2] == p or rows[0] == p:
         return True
-def Wincheck():
+def Wincheck(m):
     Board()
     b,z = 0,0
     for i in rows:
         for j in i:
             if j == x: b += 1
             if j == o: z += 1
-    if Check(1, m):
-        if soy == 1:
-            if milk == o:   print("=-=-[Computer Won in {} moves]-=-=\n".format(b))
-            else:   print("=-=-[You Won in {} moves]-=-=\n".format(b))
-        else:   print("=-=-[x Won in {} moves]-=-=\n".format(b))
-        return True
-    elif Check(0, n):
-        if soy == 1 and milk == x:
-            if milk == x:   print("=-=-[Computer Won in {} moves]-=-=\n".format(z))
-            else:   print("=-=-[You Won in {} moves]-=-=\n".format(z))
-        else:   print("=-=-[o Won in {} moves]-=-=\n".format(z))
-        return True
-    elif b == 5:
+    if m: #X then O
+        if Check(1, m):  # X
+            if soy == 1:
+                if milk == o: print("=-=-[Computer Won in {} moves]-=-=\n".format(b))
+                else: print("=-=-[You Won in {} moves]-=-=\n".format(b))
+            else: print("=-=-[x Won in {} moves]-=-=\n".format(b))
+            return True
+        elif Check(0, n):  # O
+            if soy == 1 and milk == x:
+                if milk == x: print("=-=-[Computer Won in {} moves]-=-=\n".format(z))
+                else: print("=-=-[You Won in {} moves]-=-=\n".format(z))
+            else: print("=-=-[o Won in {} moves]-=-=\n".format(z))
+            return True
+    else: #O then X
+        if Check(0, n):  # O
+            if soy == 1 and milk == x:
+                if milk == x: print("=-=-[Computer Won in {} moves]-=-=\n".format(z))
+                else: print("=-=-[You Won in {} moves]-=-=\n".format(z))
+            else: print("=-=-[o Won in {} moves]-=-=\n".format(z))
+            return True
+        elif Check(1, m):  # X
+            if soy == 1:
+                if milk == o: print("=-=-[Computer Won in {} moves]-=-=\n".format(b))
+                else: print("=-=-[You Won in {} moves]-=-=\n".format(b))
+            else: print("=-=-[x Won in {} moves]-=-=\n".format(b))
+            return True
+    if b == 5:
         print("  Tie\n")
         return True
 def Computer(salmon,max):
@@ -51,12 +65,12 @@ def Computer(salmon,max):
                 break
 e,x,o,m,n = "_","X","O", ["X","X","X"], ["O","O","O"]
 milk,p1 = "",True
-soy = int(input("\nWelcome to TicTacToe,\nAt any time Type stop to save and exit.\nChoose: \n1. User Vs Computer \n2. User Vs User \n3. Resume Old Game\nChoose 1/2/3: "))
+soy = int(input("\nWelcome to TicTacToe,\nYou can type stop to save and exit at any time.\nChoose: \n1. User Vs Computer \n2. User Vs User \n3. Resume Old Game\nChoose (1/2/3): "))
 rows = [[e,e,e],[e,e,e],[e,e,e]]
 while True:
     p1 = True
     if soy == 1:
-        milk = input("Begin with X or O? ").upper()
+        milk = input("Play as X or O? ").upper()
         if milk == o:
             Computer(x, 5)
             p1 = False
@@ -81,17 +95,14 @@ while True:
         if rows[a][b] == e:
             if p1 == True:
                 rows[a][b] = x
-                if soy == 1 and milk == x:
-                    Computer(o,4)
-                else:
-                    p1 = False
-                if Wincheck():
-                    break
+                if soy == 1 and milk == x: Computer(o,4)
+                else: p1 = False
+                if Wincheck(True): break #X then O
             elif p1 == False:
                 rows[a][b] = o
                 if soy == 1 and milk == o: Computer(x,5)
                 else: p1 = True
-                if Wincheck(): break
+                if Wincheck(False): break #O then X
             continue
         else:
             print("Unfortunately, the ({},{}) move you entered is already occupied, please try again".format(sushi[0],sushi[1]))
